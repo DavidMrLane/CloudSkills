3828e105c2d8bb2eec4082839bbd9d35bebc3aeb
David Lane <david.mr.lane@gmail.com>
2021-04-26 20:20:45 +0100

    AWStfstate

--- Week-5-CICD/terraform-ghactions/AWStfstate.sh
+++ Week-5-CICD/terraform-ghactions/AWStfstate.sh
@@ -1,2 +1,2 @@
 #Create Storage Container
-aws s3api create-bucket --bucket $1 --region ${{ env.AWS-REGION}} --create-bucket-configuration LocationConstraint=${{ env.AWS-REGION}}
+aws s3api create-bucket --bucket $1 --region $2 --create-bucket-configuration LocationConstraint=$2
